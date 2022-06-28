c5acddda92139b0924d04c30863ab7eefa0da345
SANDEEP KUMAR <107092701+sandeeprairai@users.noreply.github.com>
2022-06-28 20:34:15 +0530

    Create basic2.py

---
+++ basic2.py
@@ -0,0 +1,38 @@
+##tuple -immutable 
+emp=()
+print(type(emp))
+city="Pune",
+type(city)
+city=("Pune","Banglore","Delhi","Mumbai")
+city
+list1=[1,2,3,4]
+tuple1=(1,2,3,4)
+list1.append(5)
+list1
+# tuple1.append(5)
+print(city)
+city[1]
+city[-1]
+num=1,2,3,4
+print(city+num)
+nest=(city,num)
+print(nest)
+rep=("python",)*5
+print(rep)
+num
+num[1:]
+num[::-1]
+tuple("SandeepRai")
+a,b,c,d=num
+print(a,b,c,d)
+a,*b,c=num
+print(a,b,c)
+tuple1=(1,2,3,4)
+print(tuple1)
+del tuple1
+num1=(3,5,3,2,7,8,9,6,4,3)
+num1.count(3)
+sum(num1)
+len(num1)
+max(num1)
+min(num1)
