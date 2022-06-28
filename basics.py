bc6168adf7a343cd06a5acc63306f48a13bb5824
SANDEEP KUMAR <107092701+sandeeprairai@users.noreply.github.com>
2022-06-28 20:30:30 +0530

    Create basics.py

---
+++ basics.py
@@ -0,0 +1,42 @@
+num=[1,2,3,4]
+print(num)
+letter=['a','b','c','d']
+print(letter)
+stg=["get","certified","get","ahead"]
+print(stg)
+mix=[1,6,"Sandeep","get","certified"]
+print(mix)
+mat=[[1,2],['a','b']]
+print(mat)
+#accessing th element of list
+print(mix)
+mix[3]
+mix[-3]
+mix[:3]
+mix[3:]
+mix[::2]
+
+#operation on list
+z=[0]*100
+print(z)
+print(letter)
+print(stg)
+conc =letter+stg
+print(conc)
+var=list("hyy there")
+print(var)
+print(num)
+one,*other=num
+print(one)
+print(other)
+#method
+print(num)
+num.append(6)
+print(num)
+num.extend(stg)
+print(stg)
+print(num)
+num.insert(5,"Karan")
+print(num)
+num.remove("Karan")
+print(num)
